7bac5361bb597de8b327a8678f062a9658ac5729
PALLAVI <166042981+14PALLAVI-14@users.noreply.github.com>
2025-03-24 18:24:16 +0530

    Update App.py

--- App.py
+++ App.py
@@ -61,8 +61,3 @@ if st.button("Generate Design"):
            st.warning("No relevant images found on Lexica.art.")
    else:
       st.warning("Please fill in all the fields.")
-
-You can now view your Streamlit app in your browser.
-Local URL: http://localhost:8501
-Network URL: http://192.168.0.6:8501
-
